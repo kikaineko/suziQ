de41a1e91b3bdf1f6b322dd92f6351a14a40878d
kikaineko <kikainekox@gmail.com>
2011-04-28 17:07:28 +0900

    sample

---
+++ sample/js/jquery.suziQ.settings.js
@@ -0,0 +1,13 @@
+(function(jQuery) {
+	/**
+ 	* setting is open to user.
+ 	*/
+	jQuery.sq.settings.message_charset='UTF-8';
+	jQuery.sq.settings.default_locale='ja';
+	jQuery.sq.settings.locale_path='/lang';
+	
+	jQuery.sq.settings.ajax_dataType = "text";
+	
+	jQuery.sq.settings.context_root_on = false;
+})(jQuery);
+	
