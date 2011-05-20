427173cab2734f3b5622f3317bbac7c0c76d2c42
kikaineko <kikainekox@gmail.com>
2011-05-20 13:15:57 +0900

    aa

---
+++ src/jquery.suziQ.settings.js
@@ -0,0 +1,9 @@
+(function(jQuery) {
+	/**
+ 	* setting is open to user.
+ 	*/
+	jQuery.sq.settings.message_charset='UTF-8';
+	jQuery.sq.settings.default_locale='en';
+	jQuery.sq.settings.locale_path='/lang';
+})(jQuery);
+	
